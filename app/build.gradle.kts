2a2601012c81569f63719d12abee25b648fd0ee5
FranciscoCarvalho2001 <101589943+FranciscoCarvalho2001@users.noreply.github.com>
2023-11-25 16:39:48 +0000

    Criação SplashScreen +layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,13 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    sourceSets {
+        getByName("main") {
+            assets {
+                srcDirs("src\\main\\assets", "src\\main\\assets")
+            }
+        }
+    }
 }
 
 dependencies {
@@ -44,4 +51,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.airbnb.android:lottie:5.2.0")
 }
