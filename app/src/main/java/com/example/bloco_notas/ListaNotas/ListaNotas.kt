dfcdf3a449cbb0c80757edfa8a696f3c79eadcd2
DiniSilva <101589841+DiniSilva@users.noreply.github.com>
2023-11-26 16:25:19 +0000

    Class Nota + Adapter adicionado
    Adapter da ListaNotas e layout das Notas

--- app/src/main/java/com/example/bloco_notas/ListaNotas/ListaNotas.kt
+++ app/src/main/java/com/example/bloco_notas/ListaNotas/ListaNotas.kt
@@ -1,9 +1,11 @@
-package com.example.bloco_notas
+package com.example.bloco_notas.ListaNotas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.bloco_notas.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ListaNotas : AppCompatActivity() {
@@ -11,18 +13,12 @@ class ListaNotas : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_notas)
-        // use arrayadapter and define an array
-        val arrayAdapter: ArrayAdapter<*>
+
         var user :ArrayList<String> = arrayListOf( "Nota1","Nota2","Nota3","Nota4","Nota5","Nota6")
-        // Acessa
-        var mListView = findViewById<ListView>(R.id.listaNotas)
-        arrayAdapter = ArrayAdapter(this,
-            android.R.layout.simple_list_item_1, user)
-        mListView.adapter = arrayAdapter
+
         val fab: FloatingActionButton = findViewById(R.id.Adicionar)
         fab.setOnClickListener(){
             adicionar(user)
-            mListView.adapter = arrayAdapter
         }
     }
 
